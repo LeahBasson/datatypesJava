78a6c2a989c4bbccc165151cf256f1d005a37525
LeahBasson <leahbasson54@gmail.com>
2025-03-04 18:40:30 +0200

    java datatypes

---
+++ datatypes/src/datatypes/Datatypes.java
@@ -0,0 +1,31 @@
+
+package datatypes;
+
+
+public class Datatypes {
+
+    
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+        
+        String greeting="hello";
+        String person="my friend";
+        
+        double decimal1=10;
+        double decimal2=20;
+        
+        char symbol='#';
+        char initial='L';
+        
+        boolean yes=true;
+        boolean no=false;
+        
+        System.out.println(x + " "+y);
+        System.out.println(greeting+ " "+person);
+        System.out.println(decimal1 + " "+decimal2);
+        System.out.println(symbol + " "+initial);
+        System.out.println(yes + " "+no);
+    }
+    
+}
